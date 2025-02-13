3d73250e2a2cb2f65adb8bd28f2e4788821d3cb2
drkwitht <derektan3510@gmail.com>
2025-02-12 20:39:55 -0800

    Added socket wrappers.

---
+++ includes/mysock/sockets.hpp
@@ -0,0 +1,185 @@
+#pragma once
+
+// #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "meta/helpers.hpp"
+#include "mysock/buffers.hpp"
+#include "mysock/configure.hpp"
+
+namespace MyHttpd::MySock {
+    enum class SockSetupStatus {
+        ok,
+        bad_fd,
+        bad_option
+    };
+
+    enum class SockIOStatus {
+        ok,
+        closed_pipe,
+        invalid_size,
+        exhausted_buffer
+    };
+
+    class ServerSocket {
+    private:
+        static constexpr auto dud_value = -1;
+        int m_fd;
+        bool m_ready;
+
+        [[nodiscard]] SockSetupStatus applyOptions(long listen_timeout) noexcept;
+
+    public:
+        ServerSocket() noexcept;
+        ServerSocket(int fd, long listen_timeout) noexcept;
+        ~ServerSocket() noexcept;
+
+        ServerSocket(const ServerSocket& other) = delete;
+        ServerSocket& operator=(const ServerSocket& other) = delete;
+        ServerSocket(ServerSocket&& x_other) noexcept;
+        ServerSocket& operator=(ServerSocket&& x_other) noexcept;
+
+        [[nodiscard]] std::optional<int> acceptConnection() noexcept;
+    };
+
+    class ClientSocket {
+    private:
+        static constexpr auto dud_value = -1;
+
+        int m_fd;
+        bool m_closed;
+
+        [[maybe_unused]] SockSetupStatus applyOptions(long recv_timeout) noexcept;
+
+    public:
+        ClientSocket() noexcept;
+        ClientSocket(int fd, long recv_timeout) noexcept;
+        ~ClientSocket() noexcept;
+
+        ClientSocket(const ClientSocket& other) = delete;
+        ClientSocket& operator=(const ClientSocket& other) = delete;
+        ClientSocket(ClientSocket&& x_other) noexcept;
+        ClientSocket& operator=(ClientSocket&& x_other) noexcept;
+
+        template <typename OctetT, std::size_t BufferN> requires (Meta::is_buffer_item_v<OctetT>)
+        [[nodiscard]] SockIOStatus readLine(FixedBuffer<OctetT, BufferN>& target, OctetT delim) noexcept {
+            target.reset();
+
+            auto residue_space = BufferN;
+            auto read_n = 0UL;
+            OctetT temp = OctetT {};
+            bool found_delim = false;
+
+            while (not m_closed and residue_space > 0) {
+                auto temp_read_n = recv(m_fd, &temp, 1UL, 0);
+
+                if (temp_read_n <= 0) {
+                    m_closed = true;
+                    return SockIOStatus::closed_pipe;
+                }
+
+                if (temp == delim) {
+                    found_delim = true;
+                    break;
+                }
+
+                if (delim == '\n' and temp == '\r') {
+                    continue;
+                }
+
+                target.getPtr()[read_n] = temp;
+
+                ++read_n;
+                --residue_space;
+            }
+
+            if (not found_delim and not residue_space) {
+                return SockIOStatus::exhausted_buffer;
+            }
+
+            return SockIOStatus::ok;
+        }
+
+        template <typename OctetT, std::size_t BufferN> requires (Meta::is_buffer_item_v<OctetT>)
+        [[nodiscard]] SockIOStatus readBlob(const FixedBuffer<OctetT, BufferN>& target, std::size_t blob_size) noexcept {
+            if (blob_size > BufferN) {
+                return SockIOStatus::invalid_size;
+            }
+
+            target.reset();
+
+            auto pending_n = blob_size;
+            auto done_n = 0UL;
+            long temp_n = 0L;
+
+            while (not m_closed and pending_n > 0UL) {
+                temp_n = recv(m_fd, target.getPtr() + done_n, pending_n, 0);
+
+                if (temp_n <= 0) {
+                    m_closed = true;
+                    target.reset();
+
+                    return SockIOStatus::closed_pipe;
+                }
+
+                done_n += temp_n;
+                pending_n -= temp_n;
+            }
+
+            return (pending_n == 0UL) ? SockIOStatus::ok : SockIOStatus::closed_pipe;
+        }
+
+        template <typename OctetT, std::size_t BufferN>
+        [[nodiscard]] SockIOStatus writeBlob(const FixedBuffer<OctetT, BufferN>& source) noexcept {
+            auto pending_n = source.getLength();
+
+            if (pending_n == 0UL) {
+                return SockIOStatus::invalid_size;
+            }
+
+            auto done_n = 0UL;
+
+            while (not m_closed and pending_n > 0UL) {
+                auto temp_n = send(m_fd, source.getPtr() + done_n, pending_n, 0);
+
+                if (temp_n <= 0L) {
+                    m_closed = true;
+                    source.reset();
+                    return SockIOStatus::closed_pipe;
+                }
+
+                done_n += temp_n;
+                pending_n -= temp_n;
+            }
+
+            return (pending_n == 0UL) ? SockIOStatus::ok : SockIOStatus::closed_pipe;
+        }
+
+        template <typename OctetT, std::size_t BufferN>
+        [[nodiscard]] SockIOStatus writeBlob(const FixedBuffer<OctetT, BufferN>& source, std::size_t N) noexcept {
+            auto pending_n = N;
+
+            if (N == 0UL or N > source.getLength()) {
+                return SockIOStatus::invalid_size;
+            }
+
+            auto done_n = 0UL;
+
+            while (not m_closed and pending_n > 0UL) {
+                auto temp_n = send(m_fd, source.getPtr() + done_n, pending_n, 0);
+
+                if (temp_n <= 0L) {
+                    m_closed = true;
+                    source.reset();
+                    return SockIOStatus::closed_pipe;
+                }
+
+                done_n += temp_n;
+                pending_n -= temp_n;
+            }
+
+            return (pending_n == 0UL) ? SockIOStatus::ok : SockIOStatus::closed_pipe;
+        }
+    };
+}
---
+++ src/mysock/sockets.cpp
@@ -0,0 +1,132 @@
+#include <unistd.h>
+#include <utility>
+#include "mysock/sockets.hpp"
+
+namespace MyHttpd::MySock {
+    SockSetupStatus ServerSocket::applyOptions(long listen_timeout) noexcept {
+        if (m_fd == dud_value) {
+            return SockSetupStatus::bad_fd;
+        }
+
+        timeval timeout_secs {
+            .tv_sec = listen_timeout,
+            .tv_usec = 0
+        };
+
+        if (setsockopt(m_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout_secs, sizeof(timeval)) == dud_value) {
+            return SockSetupStatus::bad_option;
+        }
+
+        return SockSetupStatus::ok;
+    }
+
+    ServerSocket::ServerSocket() noexcept
+    : m_fd {dud_value}, m_ready {false} {}
+
+    ServerSocket::ServerSocket(int fd, long listen_timeout) noexcept
+    : m_fd {fd}, m_ready {applyOptions(listen_timeout) == SockSetupStatus::ok} {}
+
+    ServerSocket::~ServerSocket() noexcept {
+        if (m_fd != dud_value) {
+            close(m_fd);
+            m_fd = dud_value;
+        }
+    }
+
+    ServerSocket::ServerSocket(ServerSocket&& x_other) noexcept
+    : m_fd {dud_value}, m_ready {false} {
+        if (&x_other == this) {
+            return;
+        }
+
+        if (m_fd != dud_value) {
+            close(m_fd);
+        }
+
+        m_fd = std::exchange(x_other.m_fd, dud_value);
+        m_ready = std::exchange(x_other.m_ready, false);
+    }
+
+    ServerSocket& ServerSocket::operator=(ServerSocket&& x_other) noexcept {
+        if (&x_other == this) {
+            return *this;
+        }
+
+        if (m_fd != dud_value) {
+            close(m_fd);
+        }
+
+        m_fd = std::exchange(x_other.m_fd, dud_value);
+        m_ready = std::exchange(x_other.m_ready, false);
+
+        return *this;
+    }
+
+    std::optional<int> ServerSocket::acceptConnection() noexcept {
+        if (not m_ready) {
+            return {};
+        }
+
+        auto temp_fd = accept(m_fd, nullptr, nullptr);
+
+        if (temp_fd == dud_value) {
+            return {};
+        }
+
+        return {temp_fd};
+    }
+
+
+    SockSetupStatus ClientSocket::applyOptions(long recv_timeout) noexcept {
+        if (m_fd == dud_value) {
+            return SockSetupStatus::bad_fd;
+        }
+    
+        timeval timeout_secs {
+            .tv_sec = recv_timeout,
+            .tv_usec = 0
+        };
+    
+        if (setsockopt(m_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout_secs, sizeof(timeval)) == dud_value) {
+            return SockSetupStatus::bad_option;
+        }
+    
+        return SockSetupStatus::ok;
+    }
+
+    ClientSocket::ClientSocket() noexcept
+    : m_fd {dud_value}, m_closed {true} {}
+
+    ClientSocket::ClientSocket(int fd, long recv_timeout) noexcept
+    : m_fd {fd}, m_closed {false} {
+        applyOptions(recv_timeout);
+    }
+
+    ClientSocket::~ClientSocket() noexcept {
+        if (m_fd != dud_value) {
+            close(m_fd);
+            m_fd = dud_value;
+        }
+    }
+
+    ClientSocket::ClientSocket(ClientSocket&& x_other) noexcept
+    : m_fd {dud_value}, m_closed {true} {
+        m_fd = std::exchange(x_other.m_fd, dud_value);
+        m_closed = std::exchange(x_other.m_closed, true);
+    }
+
+    ClientSocket& ClientSocket::operator=(ClientSocket&& x_other) noexcept {
+        if (&x_other == this) {
+            return *this;
+        }
+
+        if (m_fd != dud_value) {
+            close(m_fd);
+        }
+
+        m_fd = std::exchange(x_other.m_fd, dud_value);
+        m_closed = std::exchange(x_other.m_closed, true);
+
+        return *this;
+    }
+}
